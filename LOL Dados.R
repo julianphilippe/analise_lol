786c09a9b3035b8be3918c28eae2e0b03f4d2c34
julian philippe <julian.philippe@outlook.com>
2017-07-18 22:12:26 -0300

    teste

---
+++ LOL Dados.R
@@ -0,0 +1,230 @@
+
+#rm(list = ls())
+
+library(tibble)
+library(httr)
+library(glue)
+library(rvest)
+library(rjson)
+library(dplyr)
+library(lubridate)
+
+
+
+#fun??o lista de invocadores
+
+invocadores <- function(invocador){
+  
+  invocador = paste0(invocador)
+  
+  key = '?api_key=RGAPI-0467b4d0-6f12-4cf0-a051-2287ef392b53'
+  
+  urlInvoc = 'https://br.api.riotgames.com/api/lol/BR/v1.4/summoner/by-name/'
+  
+  #invoc = 'iceddeath,kuririncareca'
+  
+  listaInvoc <-rjson::fromJSON(paste0(readLines(paste0(urlInvoc,invocador,key))))
+  
+  #print(paste0(urlInvoc,invoc,key))
+  
+  dfInvoc <- tibble()
+  for (i in seq_along(listaInvoc)){
+    a <- tibble(nome = listaInvoc[[i]]$name, id = listaInvoc[[i]]$id)
+    dfInvoc <- bind_rows(dfInvoc, a)
+  }
+  assign('dfInvoc', dfInvoc, envir=.GlobalEnv)
+  
+}
+
+
+#invocadores('iceddeath,kuririncareca,pansadogueto,rodil,itsshowtime,xeternox,zEmerson,fullkira,fenixytb')
+
+
+#------------------------------------------------------------------------------------#
+
+
+
+#fun??o lista de partidas
+
+partidas <- function(df){
+  
+  urlPartidas = 'https://br.api.riotgames.com/api/lol/BR/v2.2/matchlist/by-summoner/'
+  
+  key = '?seasons=PRESEASON2016&seasons=SEASON2016&seasons=PRESEASON2017&seasons=SEASON2017&api_key=RGAPI-0467b4d0-6f12-4cf0-a051-2287ef392b53'
+  
+  dfPartidas <- tibble()
+  
+  for (i in seq_along(df)){
+    
+    id <- df[i]
+    
+    idInvoc = id
+    
+    listaPartidas <- rjson::fromJSON(paste0(readLines(paste0(urlPartidas,idInvoc,key))))
+    
+    print(id)
+    
+    for (i in seq_along(listaPartidas$matches)){
+      a <- tibble(idJogador = id, id_partida = listaPartidas$matches[[i]]$matchId, campeao = listaPartidas$matches[[i]]$champion, lane = listaPartidas$matches[[i]]$lane, 
+                  posicao = listaPartidas$matches[[i]]$role, temporada = listaPartidas$matches[[i]]$season)
+      dfPartidas <- bind_rows(dfPartidas, a)
+    }
+    
+  }
+  assign('dfPartidas', dfPartidas, envir=.GlobalEnv)
+}
+
+
+#partidas(dfInvoc$id)
+
+dfInvoc[2]
+
+#------------------------------------------------------------------------------------#
+
+#partidas das ?ltimas duas temporadas
+
+
+dadosPartidas <- function(df){
+  
+  urlPartida = 'https://br1.api.riotgames.com/lol/match/v3/matches/'
+  
+  #idPartida = '1128639786'
+  
+  key = '?api_key=RGAPI-0467b4d0-6f12-4cf0-a051-2287ef392b53'
+  
+  geralPartidas <- tibble()
+  
+  for (i in seq_along(df)){
+    
+    id = df[i]
+    
+    idPartida = id
+    
+    print(i)
+    
+    listaDadosPart <- rjson::fromJSON(paste0(readLines(paste0(urlPartida,idPartida,key))))
+    
+    Sys.sleep(2)
+    
+    
+    #----------------------Status Partida-------------------------#
+    
+    geralPartidaTime_1 <- tibble(gameId = listaDadosPart$gameId,
+                                 tempoPartidaMin = seconds_to_period(listaDadosPart$gameDuration)@minute,
+                                 gameMode = listaDadosPart$gameMode,
+                                 gameVersion = listaDadosPart$gameVersion,
+                                 teamId = listaDadosPart$teams[[1]]$teamId,
+                                 win = listaDadosPart$teams[[1]]$win,
+                                 firstBlood = listaDadosPart$teams[[1]]$firstBlood,
+                                 firstTower = listaDadosPart$teams[[1]]$firstTower,
+                                 firstInhibitor =listaDadosPart$teams[[1]]$firstInhibitor,
+                                 firstBaron = listaDadosPart$teams[[1]]$firstBaron,
+                                 firstDragon = listaDadosPart$teams[[1]]$firstDragon,
+                                 firstRiftHerald = listaDadosPart$teams[[1]]$firstRiftHerald,
+                                 towerKills = listaDadosPart$teams[[1]]$towerKills,
+                                 inhibitorKills = listaDadosPart$teams[[1]]$inhibitorKills,
+                                 baronKills = listaDadosPart$teams[[1]]$baronKills,
+                                 dragonKills = listaDadosPart$teams[[1]]$dragonKills,
+                                 vilemawKills = listaDadosPart$teams[[1]]$vilemawKills,
+                                 riftHeraldKills = listaDadosPart$teams[[1]]$riftHeraldKills)
+    
+    
+    geralPartidaTime_2 <- tibble(gameId = listaDadosPart$gameId,
+                                 tempoPartidaMin = seconds_to_period(listaDadosPart$gameDuration)@minute,
+                                 gameMode = listaDadosPart$gameMode,
+                                 gameVersion = listaDadosPart$gameVersion,
+                                 teamId = listaDadosPart$teams[[2]]$teamId,
+                                 win = listaDadosPart$teams[[2]]$win,
+                                 firstBlood = listaDadosPart$teams[[2]]$firstBlood,
+                                 firstTower = listaDadosPart$teams[[2]]$firstTower,
+                                 firstInhibitor =listaDadosPart$teams[[2]]$firstInhibitor,
+                                 firstBaron = listaDadosPart$teams[[2]]$firstBaron,
+                                 firstDragon = listaDadosPart$teams[[2]]$firstDragon,
+                                 firstRiftHerald = listaDadosPart$teams[[2]]$firstRiftHerald,
+                                 towerKills = listaDadosPart$teams[[2]]$towerKills,
+                                 inhibitorKills = listaDadosPart$teams[[2]]$inhibitorKills,
+                                 baronKills = listaDadosPart$teams[[2]]$baronKills,
+                                 dragonKills = listaDadosPart$teams[[2]]$dragonKills,
+                                 vilemawKills = listaDadosPart$teams[[2]]$vilemawKills,
+                                 riftHeraldKills = listaDadosPart$teams[[2]]$riftHeraldKills)
+    
+    a <- bind_rows(geralPartidaTime_1, geralPartidaTime_2)
+    
+    geralPartidas <- bind_rows(geralPartidas, a)
+    
+    participantes <- tibble()
+    
+    for (i in seq_along(listaDadosPart$participants)){
+      a <- tibble(gameId = listaDadosPart$gameId,
+                  teamId = listaDadosPart$participants[[i]]$teamId,
+                  win = listaDadosPart$participants[[i]]$stats$win,
+                  participantId = listaDadosPart$participants[[i]]$participantId,
+                  championId = listaDadosPart$participants[[i]]$championId,
+                  spell1Id = listaDadosPart$participants[[i]]$spell1Id,
+                  spell2Id = listaDadosPart$participants[[i]]$spell2Id,
+                  highestAchievedSeasonTier = listaDadosPart$participants[[i]]$highestAchievedSeasonTier,
+                  role = listaDadosPart$participants[[i]]$timeline$role,
+                  lane = listaDadosPart$participants[[i]]$timeline$lane,
+                  kills = listaDadosPart$participants[[i]]$stats$kills,
+                  deaths = listaDadosPart$participants[[i]]$stats$deaths,
+                  assists = listaDadosPart$participants[[i]]$stats$assists,
+                  killingSprees = listaDadosPart$participants[[i]]$stats$killingSprees,
+                  inhibitorKills = listaDadosPart$participants[[i]]$stats$inhibitorKills,
+                  turretKills = listaDadosPart$participants[[i]]$stats$turretKills,
+                  totalMinionsKilled = listaDadosPart$participants[[i]]$stats$totalMinionsKilled,
+                  goldEarned = listaDadosPart$participants[[i]]$stats$goldEarned,
+                  champLevel = listaDadosPart$participants[[i]]$stats$champLevel,
+                  totalDamageDealt = listaDadosPart$participants[[i]]$stats$totalDamageDealt,
+                  magicDamageDealt = listaDadosPart$participants[[i]]$stats$magicDamageDealt,
+                  physicalDamageDealt = listaDadosPart$participants[[i]]$stats$physicalDamageDealt,
+                  trueDamageDealt = listaDadosPart$participants[[i]]$stats$trueDamageDealt,
+                  longestTimeSpentLiving = listaDadosPart$participants[[i]]$stats$longestTimeSpentLiving,
+                  totalTimeCrowdControlDealt = listaDadosPart$participants[[i]]$stats$totalTimeCrowdControlDealt,
+                  visionScore = listaDadosPart$participants[[i]]$stats$visionScore)
+      
+      participantes <- bind_rows(participantes, a)
+      
+    }
+    
+  }
+  
+  assign('geralPartidas', geralPartidas, envir=.GlobalEnv)
+  assign('participantes', participantes, envir=.GlobalEnv)
+  
+  
+}
+
+#dadosPartidas(dfPartidas$id_partida)
+
+#----------------------Status Participantes-------------------------#
+
+seq_along(dfPartidas$id_partida)
+
+rm(list = 'i')
+
+
+
+
+#adc <- participantes %>%
+#  filter(role == 'DUO_CARRY')
+
+#sup <- participantes %>%
+#  filter(role == 'DUO_SUPPORT')
+
+#jungle <- participantes %>%
+#  filter(lane == 'JUNGLE')
+
+#mid <- participantes %>%
+#  filter(lane == 'MIDDLE')
+
+#top <- participantes %>%
+#  filter(lane == 'TOP')
+
+#-----------------------------------------------------------#
+teste <- geralPartidas %>%
+  inner_join(participantes, by = c('gameId' = 'gameId', 'teamId' = 'teamId'))
+
+dfDadosPart <- tibble()
+
+
+rm(list = 'adc')
