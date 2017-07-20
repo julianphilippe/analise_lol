95ed459af6febc47f612f838ea3421321b6cc1a5
Julian Philippe Monteiro Mussumeci <julian.mussumeci@corp.nova>
2017-07-20 13:15:26 -0300

    Ajuste nomes dfs

--- tratamentoDados.R
+++ tratamentoDados.R
@@ -14,7 +14,11 @@ library(readr)
 #participantes <- readr::read_delim('participantes.csv', delim = ';')
 
 
-names(participantes)
+
+#----------------------separa dados dos participantes por times---------------------#
+
+
+#----ADC----#
 
 adc <- participantes %>%
   filter(role == 'DUO_CARRY') %>%
@@ -23,6 +27,7 @@ adc <- participantes %>%
          magicDamageDealt, physicalDamageDealt, trueDamageDealt, longestTimeSpentLiving, totalTimeCrowdControlDealt, 
          visionScore) 
 
+#----SUP---#
 
 sup <- participantes %>%
   filter(role == 'DUO_SUPPORT') %>%
@@ -31,12 +36,17 @@ sup <- participantes %>%
          magicDamageDealt, physicalDamageDealt, trueDamageDealt, longestTimeSpentLiving, totalTimeCrowdControlDealt, 
          visionScore) 
 
+#----JG----#
+
 jungle <- participantes %>%
   filter(lane == 'JUNGLE') %>%
   select(gameId, teamId, championId, spell1Id, spell2Id, highestAchievedSeasonTier, role, lane, kills, deaths, assists,
          killingSprees, inhibitorKills, turretKills, totalMinionsKilled, goldEarned, champLevel, totalDamageDealt, 
          magicDamageDealt, physicalDamageDealt, trueDamageDealt, longestTimeSpentLiving, totalTimeCrowdControlDealt, 
          visionScore) 
+
+
+#----MID----#
 
 mid <- participantes %>%
   filter(lane == 'MIDDLE') %>%
@@ -45,6 +55,9 @@ mid <- participantes %>%
          magicDamageDealt, physicalDamageDealt, trueDamageDealt, longestTimeSpentLiving, totalTimeCrowdControlDealt, 
          visionScore) 
 
+
+#----TOP----#
+
 top <- participantes %>%
   filter(lane == 'TOP') %>%
   select(gameId, teamId, championId, spell1Id, spell2Id, highestAchievedSeasonTier, role, lane, kills, deaths, assists,
@@ -52,7 +65,73 @@ top <- participantes %>%
          magicDamageDealt, physicalDamageDealt, trueDamageDealt, longestTimeSpentLiving, totalTimeCrowdControlDealt, 
          visionScore) 
 
+#--------------------------ajusta nomes---------------------------#
+
+
+#----ADC----#
+
+nomes <- names(adc[3:24])
+
+nome_adc <- paste0('adc_',nomes)
+
+names(adc) <- c('gameId', 'teamId', nome_adc)
+
+
+#----SUP----#
+
+nomes <- names(sup[3:24])
+
+nome_sup <- paste0('sup_',nomes)
+
+names(sup) <- c('gameId', 'teamId', nome_sup)
+
+
+#----JG----#
+
+nomes <- names(jungle[3:24])
+
+nome_jg <- paste0('jg_',nomes)
+
+names(jungle) <- c('gameId', 'teamId', nome_jg)
+
+
+#----MID----#
+
+nomes <- names(mid[3:24])
+
+nome_mid <- paste0('mid_',nomes)
+
+names(mid) <- c('gameId', 'teamId', nome_mid)
+
+
+#----TOP----#
+
+nomes <- names(top[3:24])
+
+nome_top <- paste0('top_',nomes)
+
+names(top) <- c('gameId', 'teamId', nome_top)
+
+
+#-----Apaga listas de nomes------#
+
+rm(list = c('nomes','nome_top','nome_adc','nome_mid','nome_jg','nome_sup'))
+
+
+
+#--------------------Cria tabela final--------------------#
+
 
 partidasFinal <- geralPartidas %>%
   left_join(adc, by = c('gameId' = 'gameId', 'teamId' = 'teamId'))
+
+
+
+
+
+
+
+
+
+
 
